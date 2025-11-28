940f171f6384bc61f18bf6b3431f135a6f443fb3
Aldemir S <aldxmir.s2@gmail.com>
2025-11-28 22:01:43 +0000

    Somente no período da Black Friday (_for element p_)

--- src/components/resultado/results-page-client.tsx
+++ src/components/resultado/results-page-client.tsx
@@ -239,7 +239,7 @@ export function ResultsPageClient({ results }: ResultsPageClientProps) {
                             <CardTitle className="font-headline text-xl md:text-2xl text-black flex items-center"><Gift className="mr-2"/> Seus Bônus Exclusivos</CardTitle>
                         </CardHeader>
                         <CardContent>
-                            <p className="mb-4 text-foreground/80">APENAS SE VOCÊ GARANTIR NO PERÍODO DA BLACK FRIDAY</p>
+                            <p className="mb-4 text-foreground/80">Somente no período da Black Friday</p>
                              <ul className="space-y-4">
                                 {bonusContent.map(bonus => (
                                     <li key={bonus.id} className="flex items-start">
@@ -357,5 +357,7 @@ export function ResultsPageClient({ results }: ResultsPageClientProps) {
     </div>
   );
 }
+
+    
 
     
